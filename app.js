bd727e772b8911d5c8bdbbc77603326d98f6be12
Omegamark <grant.mark@gmail.com>
2017-01-22 18:08:21 -0700

    changed terminology to fit reddit clone and added navbar

--- app.js
+++ app.js
@@ -1,6 +1,58 @@
 (function() {
   'use strict'
   angular
-    .module('app', ['ngMessage'])
+    .module('app', ['ngMessages'])
+    .component('redditClone', {
+      controller: function() {
+        const vm = this
+        vm.post = {};
+        vm.posts = [];
+
+        vm.$onInit = function () {
+          console.log('Hello Hi!!!');
+            setInitialPosts();
+
+
+              vm.$onInit = function () {
+                console.log('Hello Hi!');
+
+              }
+
+              //submit the form with ng-submit
+              vm.createPost = function () {
+                console.log('submitting')
+                vm.posts.push({
+                  title: vm.post.title,
+                  author: vm.post.author,
+                  image: vm.post.image,
+                  description: vm.post.description
+                })
+                vm.post.title = '',
+                vm.post.author = '',
+                vm.post.image = '',
+                vm.post.description = ''
+              }
+
+        }
+        // title, author, image, and description
+        function setInitialPosts() {
+          vm.posts.push({
+            title: 'Mark',
+            author: 'poop',
+            image: 'grant.mark@gmail.com',
+            description: '80203'
+          }, {
+            title: 'Jack',
+            author: 'thestack',
+            image: 'fullstackjack@gmail.com',
+            description: '80808'
+          })
+          console.log(vm.posts);
+    }
+      },
+      templateUrl: 'form.html'
+
+
+    })
 
 })()
