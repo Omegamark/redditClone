171337284dac3ab3a0a5efe1a25094ef77f58933
Omegamark <grant.mark@gmail.com>
2017-01-25 11:54:49 -0700

    added working comment counter

--- app.js
+++ app.js
@@ -18,21 +18,17 @@
             setInitialPosts();
 
 
-              vm.$onInit = function () {
-                console.log('Hello Hi!');
 
-              }
 
         //submit the form with ng-submit
         vm.createPost = function () {
-          console.log('submitting')
-          console.log(vm.post.title);
           vm.posts.push({
             title: vm.post.title,
             author: vm.post.author,
             imageURL: vm.post.imageURL,
             description: vm.post.description,
             date: new Date(),
+            votes: vm.post.votes,
             comment: {},
             comments: []
 
@@ -60,6 +56,7 @@
             imageURL: 'https://static.pexels.com/photos/7529/pexels-photo.jpeg',
             description: 'This was a good idea for a picture in the 60s.',
             date: moment('2000-10-10').format('LLLL'),
+            votes: 2,
             comments: []
           }, {
             title: 'Boring Concert',
@@ -67,51 +64,33 @@
             imageURL: 'https://static.pexels.com/photos/69212/pexels-photo-69212.jpeg',
             description: 'Why did I agree to do this???',
             date: moment('10-19-1981').format('LLLL'),
+            votes: 15,
             comments: []
           })
-          console.log(vm.posts);
-
-        }
+        };
 
 
         //Show comment textarea when the "Post a comment" button is clicked
         vm.showCommentTextArea = function (post) {
-          console.log('this is post', post);
-          console.log('clicked comment button');
           post.toggleCommentArea = !post.toggleCommentArea
-        }
-
-        //Counts the number of comments when users submit them to each post.
-        var commentCount = 0;
-
-        vm.submitComment = function (post) {
-          console.log('submitting comment');
-
-          vm.commentBool = !vm.commentBool
-
-          post.comments.push({
-            author: vm.post.newComment.author,
-            comment: vm.post.newComment.comment,
-            counter: commentCount + 1});
-          vm.post.newComment = {};
-          vm.showCommentTextArea(post);
-          // console.log('comment Array', post.comments);
-          commentCount = post.comments.length;
-          console.log(commentCount);
         };
 
-        console.log('comments', commentCount);
 
+        vm.submitComment = function (post) {
+          post.comments.push(vm.post.newComment)
+          vm.post.newComment = {};
+          vm.showCommentTextArea(post);
+        };
 
 
         vm.cancelPost = function (post) {
-          console.log('clicked the cancel button');
-          console.log(post);
           vm.post.newComment = {};
           vm.showCommentTextArea(post);
         };
 
+        vm.upVoteClass = function () {
 
+        }
 
 
         //* Was trying to hide comments, but was taking far too long to do with an ngIf.
@@ -125,7 +104,6 @@
 
       },
       templateUrl: 'form.html'
-
     })
 
 })()
